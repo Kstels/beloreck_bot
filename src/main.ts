7e56197613ac9ef1ab8481ab0c0fa5f1e95c54e3
KBarmin <kirill.barmin@behavox.com>
2021-04-18 22:23:32 -0400

    Clean up a bit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
-import * as readline from 'readline';
 import {UpdatesMonitorService} from './services/updates-monitor.service';
 
 async function bootstrap() {
@@ -11,47 +10,6 @@ async function bootstrap() {
 
     let updatesMonitorService = app.get<UpdatesMonitorService>(UpdatesMonitorService);
     updatesMonitorService.init();
-
-    let rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-
-    // let waitCommand = () => {
-    //     rl.question('>', (command) => {
-    //         if (command === ':q') {
-    //             rl.close();
-    //             process.exit(0);
-    //         }
-    //
-    //         switch (command) {
-    //             case 'deleteWebhook':
-    //                 controller.deleteWebhook().subscribe(() => {});
-    //                 break;
-    //             case 'getUpdate':
-    //                 getUpdates(controller, updateOffset);
-    //                 break;
-    //
-    //             case 'sendMessage':
-    //                 controller.sendMessage('In the middle of nowhere').subscribe((me) => {
-    //                     console.log(JSON.stringify(me.data, null, 2));
-    //                 });
-    //                 break;
-    //
-    //             case 'downloadFile':
-    //                 controller.downloadFile().subscribe();
-    //                 break;
-    //
-    //             default:
-    //                 console.log(`Unsupported command`);
-    //                 break;
-    //         }
-    //
-    //         waitCommand();
-    //     });
-    // };
-    //
-    // waitCommand();
 }
 
 bootstrap();
